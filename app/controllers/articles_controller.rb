d6c52d453aefa7032609c4915560a888cdd9e5a2
Michael Duchemin <msducheminjr@yahoo.com>
2016-12-01 22:39:59 -0500

    DRY up Article forms and move find into before_action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,18 +1,18 @@
 class ArticlesController < ApplicationController
+  before_action :find_article, only: [:show, :edit, :update, :destroy]
 
   def index 
     @articles = Article.all   
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def new
+    @article = Article.new
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def create
@@ -27,7 +27,6 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)    
       flash[:notice] = 'Article was successfully updated.'
       redirect_to @article
@@ -38,7 +37,6 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = 'Article was successfully destroyed.'
     redirect_to articles_path
@@ -47,6 +45,10 @@ class ArticlesController < ApplicationController
 private
   def article_params
     params.require(:article).permit(:title, :text)
+  end
+
+  def find_article
+    @article = Article.find(params[:id])
   end
 
 end
