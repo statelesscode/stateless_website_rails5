53a0f638f4c6136d9ac51ac4548af1eccf14dbab
Michael Duchemin <msducheminjr@yahoo.com>
2016-12-10 13:15:15 -0500

    Pull in routes.rb and db/ from rails_guide branch.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+
+  resources :articles do
+    resources :comments, only: [:create, :destroy]
+  end
+
+  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
