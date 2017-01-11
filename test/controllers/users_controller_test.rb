bd61885e0424a1c539f1c1f45a506f3bd7a0f62e
Michael Duchemin <msducheminjr@yahoo.com>
2017-01-11 18:59:53 -0500

    Add UsersController tests for show and email_subscribe

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers
+  include SignInHelper  
+
+  def setup
+    @user = users(:author)
+  end
+
+  test 'should get show' do
+    sign_in @user
+    get user_url(@user)
+    assert_response :success
+    assert_select 'h1', "author123's Profile"
+    assert_select 'dl dt', 8
+    assert_select 'dl dd', 8
+    assert_select 'dl dt', 'Username:'
+    assert_select 'dl dd', 'author123'    
+  end  
+
+  test 'should patch email_subscribe' do
+    user = users(:non_subscriber)
+    sign_in user
+    assert_equal false, user.email_subscriber
+    patch email_subscribe_path(user)
+    assert_redirected_to user_url(user) 
+    assert_equal 'Thank you for joining the Stateless Code email distibution!', flash[:notice]
+  end
+
 end
